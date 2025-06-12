c1048d3b48a0d93ac412303500324e3789bfcae2
rbada072 <rbada072@uottawa.com>
2025-06-12 18:10:35 -0400

    initialisation de react project

---
+++ memorygame/src/App.jsx
@@ -0,0 +1,14 @@
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>Jeu de memoire ici! </p>
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
